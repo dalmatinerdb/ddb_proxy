dbd48653080095ab7c2893b4da3ac065d97eca15
Heinz N. Gies <heinz@licenser.net>
2016-09-25 15:32:49 +0200

    Fix line protocol

--- apps/ddb_proxy/src/dp_line_proto.erl
+++ apps/ddb_proxy/src/dp_line_proto.erl
@@ -23,8 +23,7 @@ decode_metrics(Line,  State = #{decoder := Decoder}) ->
 decode_metric(Metric,  State = #{bucket := Bucket, ddb := C}) ->
     #{time := Time, key := Key, value := Value} = Metric,
     KeyBin = dproto:metric_from_list(Key),
-
     Points = mmath_bin:from_list([Value]),
     C1 = dp_util:ddb_c(ddb_tcp:send(KeyBin, Time, Points, C)),
-    dp_index:add(Bucket, KeyBin, Metric),
+    dp_index:add(Bucket, Metric),
     State#{ddb => C1}.
